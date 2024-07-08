3d3c7835c577c3b06358b76ef5555ee333eec7c5
justdanny0 <153352400+Young-Keun-LEE@users.noreply.github.com>
2024-07-08 08:07:39 +0000

    Make parents package

---
+++ ThreadStudy/ThreadExample3/WaitNotifyExample.java
@@ -0,0 +1,13 @@
+package ThreadStudy.ThreadExample3;
+
+public class WaitNotifyExample {
+    public static void main(String[] args) {
+        WorkObject workObject = new WorkObject();
+
+        ThreadA threadA = new ThreadA(workObject);
+        ThreadB threadB = new ThreadB(workObject);
+
+        threadA.start();
+        threadB.start();
+    }
+}
