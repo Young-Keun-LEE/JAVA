165c209f14fd1a0ec229e17ddc27d235ee3a3fbe
justdanny0 <153352400+Young-Keun-LEE@users.noreply.github.com>
2024-07-08 08:31:41 +0000

    Revert "DataStructureExample1"
    This reverts commit 51dce7af9d4c4313755da53ab8eee7b18f5442ad.

---
+++ ThreadStudy/ThreadExample1/YieldExample.java
@@ -0,0 +1,17 @@
+package ThreadStudy.ThreadExample1;
+
+public class YieldExample {
+
+    public static void main(String[] args) {
+        WorkThread workThreadA = new WorkThread("workThreadA");
+        WorkThread workThreadB = new WorkThread("workThreadB");
+        workThreadA.start();
+        workThreadB.start();
+
+        try{Thread.sleep(5000);}catch(InterruptedException e){} //main 스레드 5초 정지
+        workThreadA.work = false;
+
+        try{Thread.sleep(10000);}catch(InterruptedException e){} //main 스레드 10초 정지
+        workThreadA.work = true;
+    }
+}
