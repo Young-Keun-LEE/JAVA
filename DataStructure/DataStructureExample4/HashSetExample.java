9ffb519979ab74fd2c2c6df0e95d29530c2dc9f6
justdanny0 <153352400+Young-Keun-LEE@users.noreply.github.com>
2024-07-09 04:35:50 +0000

    DataStructureExample4

---
+++ DataStructure/DataStructureExample4/HashSetExample.java
@@ -0,0 +1,16 @@
+package DataStructure.DataStructureExample4;
+
+import java.util.*;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        
+        Set<Member> set = new HashSet<>();
+
+        set.add(new Member("홍길동", 30));
+        set.add(new Member("홍길동", 30));
+        
+        System.out.println("총 객체 수 : " + set.size());
+
+    }    
+}
